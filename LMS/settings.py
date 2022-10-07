f17637d931a841a9ee429548d61d1892e18a41e0
Roman <roman4ik19871987@gmail.com>
2022-10-07 17:18:20 +0300

    bootstrap 5 connected, base.html, all files from the students/templates and forms modified by the bootsrap components

--- LMS/settings.py
+++ LMS/settings.py
@@ -122,11 +122,14 @@ USE_I18N = True
 
 USE_TZ = True
 
+USE_L10N = False
+DATE_FORMAT = 'd M Y'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
